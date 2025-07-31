38a4fe6af242580e48acea61900bff21a20c518d
Rammstien-king <124277230+Rammstien-king@users.noreply.github.com>
2025-07-31 13:43:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,32 +1,60 @@
 import socket
 from cryptography.fernet import Fernet
 import threading
+from meta_ai_api import MetaAI
 
-fernet = Fernet('KHBYrz6d77qr2D8sb4wiPF09qOFhuQVP7vht28I-ZRk=')
-class Style():
+
+api = MetaAI() #Meta AI object
+fernet = Fernet('KHBYrz6d77qr2D8sb4wiPF09qOFhuQVP7vht28I-ZRk=')  #encryption key
+
+class Style():     #text Color class 
   RED = "\033[31m"
   GREEN = "\033[32m"
   BLUE = "\033[34m"
   RESET = "\033[0m"
 
-def recv_client(client_socket,):
+def recv_client(client_socket,):    #recieve message
     while True:
         # Receive data from the client
         data = client_socket.recv(1024).decode()
         decrypted_data = fernet.decrypt(data).decode('utf-8')
+        if decrypted_data == "###":  # this if statement handles client queries with AI
+            data = client_socket.recv(1024).decode()
+            decrypted_data = fernet.decrypt(data).decode('utf-8') #recieved client Query for AI
+            print("AI summoned: Question Asked: " + decrypted_data)
+            message_small_reply = decrypted_data + "(Please give short answer)" #Ask AI to generate small answer only
+            response = api.prompt(message=message_small_reply)
+            response_send = "META AI: "+ response['message']
+
+            print(f"{Style.GREEN}{response_send}{Style.RESET}")
+            client_socket.send(fernet.encrypt(response_send.encode()))
+        else:
+            print(f"{Style.RED}{decrypted_data}{Style.RESET}")
         if not data:
             break
-        print(f"{Style.RED}{decrypted_data}{Style.RESET}")
+        
     
 
-def send_client(client_socket):
+def send_client(client_socket):   #send message
     while True:
         # Send a response back to the client
         message = input()
-        client_socket.send(fernet.encrypt(message.encode()))
+        if message == "###":  # this if statement handles server queries with AI
+            print("You have summoned AI. Ask:")
+            message = input()
+            AI_notify = "AI summoned: Question Asked: " + message
+            client_socket.send(fernet.encrypt(AI_notify.encode()))  #server query with AI notified to client
+            message_small_reply = message + "(Please give short answer)" #Ask AI to generate small answer only
+            response = api.prompt(message=message_small_reply)
+            response_send = "META AI: "+ response['message'] 
+
+            print(f"{Style.GREEN}{response_send}{Style.RESET}")
+            client_socket.send(fernet.encrypt(response_send.encode()))
+        else:
+            client_socket.send(fernet.encrypt(message.encode()))
     
 
-def start_server():
+def start_server():  #start server
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
